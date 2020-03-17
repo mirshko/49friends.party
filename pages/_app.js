08e6583dfa4e36ec8b223051110d27a5fb3af3af
Jeff Reiner <theundeadghost@gmail.com>
2020-03-17 01:58:08 +0100

    :sparkles: Adding twitter meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,16 @@ export default function App({ Component, pageProps }) {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <link rel="icon" href="/favicon-transparent.png" type="image/png" />
+        <meta name="twitter:title" content="49 FRIENDS" />
+        <meta
+          name="twitter:description"
+          content="A collaborative party of creatives working to bring the future to you one friend at a time."
+        />
+        <meta
+          name="twitter:image"
+          content="https://49friends.party/og-light.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
 
         <link rel="preload" href="/globe.gif" as="image" />
       </Head>
