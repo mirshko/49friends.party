fe4f6b414c8676cc06209cd47708edbf56237fca
Jeff Reiner <theundeadghost@gmail.com>
2020-06-28 16:16:45 +0200

    Adding hr and list styles

--- lib.js
+++ lib.js
@@ -1,5 +1,9 @@
 export const SITE_ID = "DYVZJBMF";
 
+/**
+ * @type {import('@mdx-js/react').MDXProviderComponents}
+ */
 export const components = {
   wrapper: (props) => <div {...props} className="copy" />,
+  hr: () => <span aria-hidden>⁂</span>,
 };
