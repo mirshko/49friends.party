0105b5e4bf685c56e48c813cf5ab51375df1ae0f
Jeff Reiner <theundeadghost@gmail.com>
2021-10-15 13:20:03 +0200

    Plain old HTML

--- lib.js
+++
@@ -1,9 +0,0 @@
-export const SITE_ID = "DYVZJBMF";
-
-/**
- * @type {import('@mdx-js/react').MDXProviderComponents}
- */
-export const components = {
-  wrapper: (props) => <div {...props} className="copy" />,
-  hr: () => <span aria-hidden>⁂</span>,
-};
